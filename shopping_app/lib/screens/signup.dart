2d9f6f5be7eb1869aa1913f4259b7ee3dc6114a8
Omar Sherif Elsayad <136436176+0mar-elsayad@users.noreply.github.com>
2024-12-12 14:09:16 +0200

    finished sign-up page UI

--- shopping_app/lib/screens/signup.dart
+++ shopping_app/lib/screens/signup.dart
@@ -10,6 +10,77 @@ class SignupScreen extends StatefulWidget {
 class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+          body: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Shopping-App',
+                  style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    hintText: 'User-Name',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    hintText: 'Email Address',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green)),
+                    hintText: 'Password',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                  ),
+                ),
+                SizedBox(height: 30),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue[900]),
+                        onPressed: () {},
+                        child: Text(
+                          'Sign up',
+                          style: TextStyle(color: Colors.white),
+                        ))),
+                SizedBox(height: 30),
+                Text('already have an account?'),
+                TextButton(onPressed: () {}, child: Text('Login'))
+              ],
+            ),
+          ),
+        ),
+      )),
+    );
   }
 }
