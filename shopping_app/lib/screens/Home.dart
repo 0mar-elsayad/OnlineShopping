7e48234a3eadf4cbec1b9858f24f227c5c567c28
Maghraby28 <143381869+Maghraby28@users.noreply.github.com>
2024-12-16 20:51:11 +0200

    searching
    search and categorical

--- shopping_app/lib/screens/Home.dart
+++ shopping_app/lib/screens/Home.dart
@@ -13,36 +13,49 @@ class _HomeScreenState extends State<HomeScreen> {
     {
       'name': 'iPhone 13',
       'price': '3500',
+      'category': 'Mobiles',
       'imageUrl':
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtkScJu7uJWUM2HXMluAkmnXHMvWeSoepePg&s',
     },
     {
       'name': 'MacBook Air',
       'price': '1200',
+      'category': 'Laptops',
       'imageUrl':
           'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034,'
     },
     {
       'name': 'Apple Watch',
       'price': '400',
+      'category': 'Watches',
       'imageUrl':
           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxc_mmycGKjssaK86jb1uoEGK2POQhHLtLWA&s',
     },
     {
       'name': 'iPad Pro',
       'price': '800',
+      'category': 'Mobiles',
       'imageUrl':
           'https://m.media-amazon.com/images/I/71vg3LNWBTL._AC_SL1500_.jpg'
     },
     {
       'name': 'AirPods',
       'price': '250',
+      'category': 'Watches',
       'imageUrl':
           'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MQD83_AV2?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1660803960086',
     },
   ];
 
+  List<Map<String, String>> filteredProducts = [];
   double _totalPrice = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the filteredProducts to display all products by default
+    filteredProducts = products;
+  }
 
   void _updateTotalPrice(double price) {
     setState(() {
@@ -53,6 +66,19 @@ class _HomeScreenState extends State<HomeScreen> {
   void _decreaseTotalPrice(double price) {
     setState(() {
       _totalPrice -= price;
+    });
+  }
+
+  // Filter products based on category
+  void _filterProducts(String category) {
+    setState(() {
+      if (category == 'All') {
+        filteredProducts = products;
+      } else {
+        filteredProducts = products
+            .where((product) => product['category'] == category)
+            .toList();
+      }
     });
   }
 
@@ -71,15 +97,34 @@ class _HomeScreenState extends State<HomeScreen> {
                 "E_commerce",
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 40),
-              // Product Grid
+              const SizedBox(height: 20),
+              // New Category Buttons Section
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _filterProducts('Mobiles'),
+                    child: const Text('Mobiles'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _filterProducts('Watches'),
+                    child: const Text('Accessories'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _filterProducts('Laptops'),
+                    child: const Text('Laptops'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // Product Grid Section
               Expanded(
                 child: GridView.count(
                   crossAxisCount: 2,
                   crossAxisSpacing: 10,
                   mainAxisSpacing: 10,
                   childAspectRatio: 4.5 / 7,
-                  children: products.map((product) {
+                  children: filteredProducts.map((product) {
                     return Productwidget(
                       name: product['name']!,
                       price: product['price']!,
@@ -89,14 +134,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               // Total Price Display
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Total Price: \$$_totalPrice",
-                  style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),
             ],
           ),
         ),
